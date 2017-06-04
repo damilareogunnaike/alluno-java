41e6211f6f6c2c9850dea10c2b24ad7a82c0b3e0
damilareogunnaike <ogunnaike.damilare@gmail.com>
2017-06-04 22:51:11 +0100

    Initialized Repo

---
+++ src/main/java/com/lahray/alluno/AllunoApplication.java
@@ -0,0 +1,12 @@
+package com.lahray.alluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllunoApplication.class, args);
+	}
+}
